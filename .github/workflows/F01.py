ed3fccf14692e6f1e2d57c28079aca7002681c20
KorbanFidas2A <82803405+KorbanFidas2A@users.noreply.github.com>
2021-04-19 22:01:59 +0700

    Update F01.py

--- .github/workflows/F01.py
+++ .github/workflows/F01.py
@@ -1,36 +1,37 @@
-print("Hello, World!)
-f = open("datacontoh.csv","r")
-lines = f.readlines()
-f.close()
-print(lines)
-
-f = open("datacontoh.csv","r")
+f = open("user.csv","r")
 raw_lines = f.readlines()
 f.close()
 lines = [raw_line.replace("\n", "") for raw_line in raw_lines]
 for line in lines:
-  print(line)
-
-f = open("datacontoh.csv","r")
-raw_lines = f.readlines()
-f.close()
-lines = [raw_line.replace("\n", "") for raw_line in raw_lines]
-def convert_line_to_data(line):
-  raw_array_of_data = line.split(",")
-  array_of_data = [data.strip() for data in raw_array_of_data]
-  return array_of_data
-def convert_array_data_to_real_values(array_data):
-  arr_cpy = array_data[:]
-  for i in range(3):
-    if(i == 0):
-      arr_cpy[i] = int(arr_cpy[i])
-    elif(i == 2):
-      arr_cpy[i] = float(arr_cpy[i])
-  return arr_cpy
-raw_header = lines.pop(0)
-header = convert_line_to_data(raw_header)
-print(header)
+    raw_array_of_data = line.split(",")
+    array_of_data = [data.strip() for data in raw_array_of_data]
+datas = []
 for line in lines:
-  array_of_data = convert_line_to_data(line)
-  real_values = convert_array_data_to_real_values(array_of_data)
-  print(real_values)
+  datas.append(array_of_data)
+array = [0 for i in range (4)]
+for i in range(4):
+    if  (i == 0):
+        array[i] = input("Masukkan nama Anda: ")
+    elif (i == 1):
+        array[i] = input("Masukkan username: ")
+    elif (i == 2):
+        array[i] = input("Masukkan password: ")
+    else:
+        array[i] = input("Masukkan alamat: ")
+datas.append(array)
+print(datas)
+
+def convert_datas_to_string():
+  string_data = ",".join(array) + "\n"
+  for arr_data in datas:
+    arr_data_all_string = [str(var) for var in arr_data]
+    string_data += ",".join(arr_data_all_string)
+    string_data += "\n"
+  return string_data
+print()
+print(convert_datas_to_string())
+
+datas_as_string = convert_datas_to_string()
+f = open("user.csv", "w")
+f.write(datas_as_string)
+f.close()
