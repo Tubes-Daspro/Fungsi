34d466fae44675c48f71c65520436a6dfa943c21
KorbanFidas2A <82803405+KorbanFidas2A@users.noreply.github.com>
2021-04-21 23:57:16 +0700

    Update F01.py

--- .github/workflows/F01.py
+++ .github/workflows/F01.py
@@ -1,42 +1,12 @@
-f = open("Book1.csv", "r")
-barisdata = f.readlines()
-f.close()
-user = []
-barisan = [isi.replace("\n", " ") for isi in barisdata]
-print(barisan)
-def changeCSV(baris):
-    arraybaru = baris.split(",")
-    arraykedua = [data.strip() for data in arraybaru]
-    return arraykedua
-
-awal = barisan.pop(0)
-ubah = changeCSV(awal)
-
-for line in barisan:
-    databaru = changeCSV(line)
-    user.append(databaru)
-
-inputdata = [0 for i in range (4)]
-for i in range(4):
-    if   (i == 0):
-        inputdata[i] = input("Masukkan nama Anda: ")
-    elif (i == 1):
-        inputdata[i] = input("Masukkan username: ")
-    elif (i == 2):
-        inputdata[i] = input("Masukkan password: ")
-    else:
-        inputdata[i] = input("Masukkan alamat: ")
-user.append(inputdata)
-
-def changeType():
-    stringbaru = ",".join(ubah)
-    for barisandata2 in user:
-        stringbaru += ",".join(barisandata2)
-        stringbaru += "\n"
-    return stringbaru
-
-newstring = changeType()
-
-f = open("Book1.csv", "w")
-f.write(newstring)
-f.close()
+def Register(user):
+    data = [0 for i in range(4)]
+    for i in range (4):
+        if (i == 0):
+            data[i] = input("Masukkan nama anda: ")
+        elif(i == 1):
+            data[i] = input("Masukkan username anda: ")
+        elif(i == 2):
+            data[i] = input("Masukkan password Anda: ")
+        else:
+            data[i] = input("Masukkan alamat anda: ")
+    user.append(data)
