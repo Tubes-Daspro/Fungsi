c1e462780e8f3dc7055caa766191b003c3498780
KorbanFidas2A <82803405+KorbanFidas2A@users.noreply.github.com>
2021-04-21 21:19:08 +0700

    Update F01.py

--- .github/workflows/F01.py
+++ .github/workflows/F01.py
@@ -1,37 +1,42 @@
-f = open("user.csv","r")
-raw_lines = f.readlines()
+f = open("Book1.csv", "r")
+barisdata = f.readlines()
 f.close()
-lines = [raw_line.replace("\n", "") for raw_line in raw_lines]
-for line in lines:
-    raw_array_of_data = line.split(",")
-    array_of_data = [data.strip() for data in raw_array_of_data]
-datas = []
-for line in lines:
-  datas.append(array_of_data)
-array = [0 for i in range (4)]
+user = []
+barisan = [isi.replace("\n", " ") for isi in barisdata]
+print(barisan)
+def changeCSV(baris):
+    arraybaru = baris.split(",")
+    arraykedua = [data.strip() for data in arraybaru]
+    return arraykedua
+
+awal = barisan.pop(0)
+ubah = changeCSV(awal)
+
+for line in barisan:
+    databaru = changeCSV(line)
+    user.append(databaru)
+
+inputdata = [0 for i in range (4)]
 for i in range(4):
-    if  (i == 0):
-        array[i] = input("Masukkan nama Anda: ")
+    if   (i == 0):
+        inputdata[i] = input("Masukkan nama Anda: ")
     elif (i == 1):
-        array[i] = input("Masukkan username: ")
+        inputdata[i] = input("Masukkan username: ")
     elif (i == 2):
-        array[i] = input("Masukkan password: ")
+        inputdata[i] = input("Masukkan password: ")
     else:
-        array[i] = input("Masukkan alamat: ")
-datas.append(array)
-print(datas)
+        inputdata[i] = input("Masukkan alamat: ")
+user.append(inputdata)
 
-def convert_datas_to_string():
-  string_data = ",".join(array) + "\n"
-  for arr_data in datas:
-    arr_data_all_string = [str(var) for var in arr_data]
-    string_data += ",".join(arr_data_all_string)
-    string_data += "\n"
-  return string_data
-print()
-print(convert_datas_to_string())
+def changeType():
+    stringbaru = ",".join(ubah)
+    for barisandata2 in user:
+        stringbaru += ",".join(barisandata2)
+        stringbaru += "\n"
+    return stringbaru
 
-datas_as_string = convert_datas_to_string()
-f = open("user.csv", "w")
-f.write(datas_as_string)
+newstring = changeType()
+
+f = open("Book1.csv", "w")
+f.write(newstring)
 f.close()
