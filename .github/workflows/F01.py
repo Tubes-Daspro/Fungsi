9f7163504a39372938a2a1649e3dfb06597b42bd
KorbanFidas2A <82803405+KorbanFidas2A@users.noreply.github.com>
2021-04-25 21:07:03 +0700

    Update F01.py

--- .github/workflows/F01.py
+++ .github/workflows/F01.py
@@ -1,13 +1,17 @@
 def Register(user):
-    data = [0 for i in range(4)]
-    for i in range (4):
+    data = [0 for i in range(6)]
+    for i in range (6):
         if (i == 0):
-            data[i] = input("Masukkan nama anda: ")
+            data[i] = input("Masukkan id: ")
         elif(i == 1):
-            data[i] = input("Masukkan username anda: ")
+            data[i] = input("Masukkan username: ")
         elif(i == 2):
-            data[i] = input("Masukkan password Anda: ")
+            data[i] = input("Masukkan nama: ")
+        elif (i == 3):
+            data[i] = input("Masukkan alamat: ")
+        elif (i == 4):
+            data[i] = input("Masukkan password: ")
         else:
-            data[i] = input("Masukkan alamat anda: ")
-    user.append(data)
+            data[i] = input("Masukkan role: ")
+        user.append(data)
     return user
