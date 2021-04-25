38fb95eda0b03b6670ed3936e9884377697c453c
LopusMaximus <82805713+LopusMaximus@users.noreply.github.com>
2021-04-26 04:29:39 +0700

    Update F04.py

--- .github/workflows/F04.py
+++ .github/workflows/F04.py
@@ -1,49 +1,57 @@
+# F04 Cari Gadget Berdasarkan Tahun Ditemukan
+
 def caritahun():
-    HasilPencarian = []
+    # Mencari gadget yang tahun ditemukannya yang cocok dengan spesifikasi tahun yang diinput.
+    # Output: (array of [string,string,integer,string,integer])
+
+    # KAMUS LOKAL
+
+    # VARIABEL
+    # Thn : integer (Input tahun ditemukannya gadget yang akan dicari.)
+    # Kateg : string (Input spesifikasi terhadap tahun input (lebih besar, lebih kecil, sama dengan, dll.))
+    # i,j,a : integer (Counter untuk iterasi.)
+    # HasilPencarian : Array (Array untuk menyimpan gadget yang tahun ditemukannya cocok dengan spesifikasi input.)
+    
+    # ALGORITMA    
+    HasilPencarian = [] # Array yang mengumpulkan yang akan di output.
     while True:
-        try:
+        try: # Pengecekan apakah tahun yang dimasukkan berupa integer.
             Thn = int(input("Masukkan Tahun: "))
         except ValueError:
             print("Masukkan invalid. Masukkan yang benar.")
             continue
 
         Kateg = input("Masukkan kategori: ")
-        if Kateg == ">" or Kateg == "<" or Kateg == ">=" or Kateg == "<=" or Kateg == "=":
-            for i in range(len(x)):
-                for j in range(len(x[i])):
-                    if j == 4 and Kateg == ">":
-                        if Thn > int(x[i][j]):
-                            HasilPencarian.append(x[i])
-                    elif j == 4 and Kateg == "<":
-                        if Thn < int(x[i][j]):
-                            HasilPencarian.append(x[i])
-                    elif j == 4 and Kateg == ">=":
-                        if Thn >= int(x[i][j]):
-                            HasilPencarian.append(x[i])
-                    elif j == 4 and Kateg == "<=":
-                        if Thn <= int(x[i][j]):
-                            HasilPencarian.append(x[i])
-                    elif j == 4 and Kateg == "=":
-                        if Thn == int(x[i][j]):
-                            HasilPencarian.append(x[i])
+        if Kateg == ">" or Kateg == "<" or Kateg == ">=" or Kateg == "<=" or Kateg == "=": # Pengecekan apakah input berupa kategori valid.
+            for i in range(len(gadget)):
+                for j in range(len(gadget[i])):
+                    if j == 5 and Kateg == ">": # Pengelompokkan gadget berdasarkan spesifikasi yang diinput.
+                        if Thn > int(gadget[i][j]):
+                            HasilPencarian.append(gadget[i])
+                    elif j == 5 and Kateg == "<":
+                        if Thn < int(gadget[i][j]):
+                            HasilPencarian.append(gadget[i])
+                    elif j == 5 and Kateg == ">=":
+                        if Thn >= int(gadget[i][j]):
+                            HasilPencarian.append(gadget[i])
+                    elif j == 5 and Kateg == "<=":
+                        if Thn <= int(gadget[i][j]):
+                            HasilPencarian.append(gadget[i])
+                    elif j == 5 and Kateg == "=":
+                        if Thn == int(gadget[i][j]):
+                            HasilPencarian.append(gadget[i])
                     
             print()
             print("Hasil pencarian:")
             print()
-            if len(HasilPencarian) != 0:
+            if len(HasilPencarian) != 0: # Pengecekan apakah HasilPencarian kosong atau tidak.
                 for a in range(len(HasilPencarian)):
-                    for b in range(len(HasilPencarian[a])):
-                        if b == 0:
-                            print("Nama             : ",(HasilPencarian[a][b]))
-                        elif b == 1:
-                            print("Deskripsi        : ",(HasilPencarian[a][b]))
-                        elif b == 2:
-                            print("Jumlah           : ",(HasilPencarian[a][b]), " buah")
-                        elif b == 3:
-                            print("Rarity           : ",(HasilPencarian[a][b]))
-                        elif b == 4:
-                            print("Tahun Ditemukan  : ",(HasilPencarian[a][b]))
-                        print()
+                    print("Nama             : ", HasilPencarian[a][1])
+                    print("Deskripsi        : ", HasilPencarian[a][2])
+                    print("Jumlah           : ", HasilPencarian[a][3])
+                    print("Rarity           : ", HasilPencarian[a][4])
+                    print("Tahun Ditemukan  : ", HasilPencarian[a][5])
+                    print()
 
                 break    
             else:
