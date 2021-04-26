4554bcb9c0e96ef77b406ec29bccc2a0cbc5bf30
jundanha <82804428+jundanha@users.noreply.github.com>
2021-04-26 22:36:39 +0700

    Add files via upload

---
+++ .github/workflows/F16.py
@@ -0,0 +1,37 @@
+def help_awal():
+    print()
+    print("=======================HELP=============================")
+    print("Untuk menggunakan program Doremonangis ini, Anda harus login terlebih dahulu")
+    print("Untuk bisa login, sebelumnya Anda harus terdaftar di sistem kami")
+    print("Silahkan hubungi Admin untuk mendaftarkan diri Anda")
+    print()
+    return 
+
+def help_admin():
+    print()
+    print("=======================HELP=============================")
+    print("Register - Untuk mendaftarkan User baru di sistem ")
+    print("Pencarian Gadget Berdasarkan Rarity - Untuk mencari semua gadget berdasarkan rarity tertentu yang diinginkan")
+    print("Pencarian Gadget Berdasarkan Tahun Ditemukan - Untuk mencari semua gadget berdasarkan tahun ditemukan tertentu")
+    print("Menambah Item - Untuk menambahkan item baru ke dalam inventory")
+    print("Menghapus Gadget atau Consumable - Untuk menghapus item (Gadget atau Consumable) pada database")
+    print("Mengubah Jumlah Gadget atau Consumable pada Inventory - Melakukan perubahan jumlah suatu item pada inventory")
+    print("Melihat Riwayat Peminjaman Gadget - Untuk melihat semua riwayat peminjaman gadget yang dilakukan oleh User")
+    print("Melihat Riwayat Pengembalian Gadget - Untuk melihat semua riwayat pengembalian gadget yang dilakukan oleh User")
+    print("Melihat Riwayat Pengambilan Consumable - Untuk melihat semua riwayat pengambilan consumable yang dilakukan oleh User")
+    print("Save Data - Untuk menyimpan semua perubahan yang sudah dilakukan ke dalam suatu folder")
+    print("Exit - Untuk mengakhiri program")
+    print()
+    return
+
+def help_user():
+    print()
+    print("=======================HELP=============================")
+    print("Pencarian Gadget Berdasarkan Rarity - Untuk mencari semua gadget berdasarkan rarity tertentu yang diinginkan")
+    print("Pencarian Gadget Berdasarkan Tahun Ditemukan - Untuk mencari semua gadget berdasarkan tahun ditemukan tertentu")
+    print("Meminjam Gadget - Untuk meminjam Gadget yang tersedia")
+    print("Mengembalikan Gadget - Untuk mengembalikan Gadget yang sedang dipinjam")
+    print("Meminta Consumable - Untuk meminta item Consumable yang tersedia")
+    print("Save Data - Untuk menyimpan semua perubahan yang sudah dilakukan ke dalam suatu folder")
+    print("Exit - Untuk mengakhiri program")
+    return
