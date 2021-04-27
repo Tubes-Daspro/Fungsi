bb6567053f6a382a6a05810ab7279feabb5edd06
jundanha <82804428+jundanha@users.noreply.github.com>
2021-04-28 03:35:33 +0700

    Update doremonangisasli.py

--- .github/workflows/doremonangisasli.py
+++ .github/workflows/doremonangisasli.py
@@ -34,6 +34,18 @@ header_user, data_user = load(path.join(args.folder, "user.csv"), ";", [])
 
 print("Loading data...\n\n")
 
+def simpan():
+    folder = input("Masukkan nama folder penyimpanan: ")
+    if not path.isdir(str(folder)):
+        makedirs(folder)
+    save(data_consumable, header_consumable, ";", path.join(folder, "consumable.csv"))
+    save(data_consumable_history, header_consumable_history, ";", path.join(folder, "consumable_history.csv"))
+    save(data_gadget, header_gadget, ";", path.join(folder, "gadget.csv"))
+    save(data_user, header_user, ";", path.join(folder, "user.csv"))
+    save(data_gadget_borrow_history, header_gadget_borrow_history, ";", path.join(folder, "gadget_borrow_history.csv"))
+    save(data_gadget_return_history, header_gadget_return_history, ";", path.join(folder, "gadget_return_history.csv"))
+    return
+
 role = 0
 status_login = 0
 id_user = 0
