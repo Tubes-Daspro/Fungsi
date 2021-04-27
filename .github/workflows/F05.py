9dabeca73ade87441b2bbe05cf37f02f391a83f4
KorbanFidas2A <82803405+KorbanFidas2A@users.noreply.github.com>
2021-04-27 17:46:09 +0700

    Update F05.py

--- .github/workflows/F05.py
+++ .github/workflows/F05.py
@@ -1,62 +1,65 @@
-#Fungsi menambah item
 def tambah_item(array_data1, array_data2):
-    if(AdminStatus):
-        i = 0
+    count = 0
+    check_count = 0
+    array_checker = [0 for i in range (10)]
+    for i in range (10):
+        array_checker[i] = str(i)
+    id_baru = input("Masukkan ID yang ingin ditambahkan: ")
+    if(id_baru[0] == "G" and len(id_baru) >= 2):
         check = False
-        id_baru = input("Masukkan ID yang ingin ditambahkan: ")
-        if(id_baru[0] == "G" and len(id_baru) >= 2):
+        while(check == False):
+            found = False
             for array in array_data1:
                 if(array[0] == id_baru):
                     print("ID sudah ada.")
-                    check = True
+                    found = True
+                    id_baru = input("Masukkan ID yang ingin ditambahkan: ")
                     break
-                elif(id_baru[1] != int):
-                    print("Format ID Anda salah.")
-                    check = True
-                    break
-            while(check == True):
+            if(found == False):
+                for i in range (10):
+                    if (array_checker[i] == id_baru[1]):
+                        count = 1
+                        break
+            if(count == 0) and (found == False):
+                print("ID yang dimasukkan invalid.")
                 id_baru = input("Masukkan ID yang ingin ditambahkan: ")
-                for array in array_data1:
-                    if(array[0] == id_baru):
-                        print("ID sudah ada.")
-                        i += 1
-                if(i == 0):
-                    check = False
-            if(check == False):
-                nama = input("Masukkan nama gadget Anda: ")
-                deskripsi = input("Masukkan deskripsi: ")
-                jumlah = int(input("Masukkan jumlah: "))
-                rarity = input("Masukkan rarity item: ")
-                tahun_ditemukan = int(input("Masukkan tahun ditemukan: "))
-                array_baru = [id_baru, nama, deskripsi, jumlah, rarity, tahun_ditemukan]
-                array_data1.append(array_baru)
-        elif(id_baru[0] == "C" and len(id_baru) >= 2):
+            elif(count == 1):
+                check = True
+        if(count == 1):
+            nama = input("Masukkan nama gadget Anda: ")
+            deskripsi = input("Masukkan deskripsi: ")
+            jumlah = int(input("Masukkan jumlah: "))
+            rarity = input("Masukkan rarity item: ")
+            tahun_ditemukan = int(input("Masukkan tahun ditemukan: "))
+            array_baru = [id_baru, nama, deskripsi, jumlah, rarity, tahun_ditemukan]
+            array_data1.append(array_baru)
+    elif(id_baru[0] == "C" and len(id_baru) >= 2):
+        check = False
+        while(check == False):
+            found = False
             for array in array_data2:
                 if(array[0] == id_baru):
                     print("ID sudah ada.")
-                    check = True
+                    found = True
+                    id_baru = input("Masukkan ID yang ingin ditambahkan: ")
                     break
-                elif(id_baru[1] != int):
-                    print("Format ID Anda salah.")
-                    check = True
-                    break
-            while(check == True):
+            if(found == False):
+                for i in range (10):
+                    if (array_checker[i] == id_baru[1]):
+                        count = 1
+                        break
+            if(count == 0) and (found == False):
+                print("ID yang dimasukkan invalid.")
                 id_baru = input("Masukkan ID yang ingin ditambahkan: ")
-                for array in array_data2:
-                    if(array[0] == id_baru):
-                        print("ID sudah ada.")
-                        i += 1
-                if(i == 0):
-                    check = False
-            if(check == False):
-                nama = input("Masukkan nama consumable Anda: ")
-                deskripsi = input("Masukkan deskripsi: ")
-                jumlah = int(input("Masukkan jumlah: "))
-                rarity = input("Masukkan rarity item: ")
-                array_baru = [id_baru, nama, deskripsi, jumlah, rarity]
-                array_data2.append(array_baru)
-        else:
-            print("ID yang dimasukkan invalid.")
+            elif(count == 1):
+                check = True
+        if(count == 1):
+            nama = input("Masukkan nama consumable Anda: ")
+            deskripsi = input("Masukkan deskripsi: ")
+            jumlah = int(input("Masukkan jumlah: "))
+            rarity = input("Masukkan rarity item: ")
+            array_baru = [id_baru, nama, deskripsi, jumlah, rarity]
+            array_data2.append(array_baru)
     else:
-        print("Access Denied.)
+        print("ID yang dimasukkan invalid.")
     return array_data1, array_data2
